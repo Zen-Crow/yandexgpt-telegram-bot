a355741a251737108a1db7d1a2ee20c6c5f1f5e8
git-lab-user1 <gurovam184@gmail.com>
2024-11-01 10:05:14 +0300

    write scrips

--- unix_env.sh
+++ unix_env.sh
@@ -3,6 +3,9 @@
 export TF_VAR_cloud_id=$(yc config get cloud-id)
 export TF_VAR_folder_id=$(yc config get folder-id)
 export TF_VAR_yc_token=$(yc iam create-token)
-export TF_VAR_telegram_token=$TELEGRAM_BOT_TOKEN
+
+read -p "Введите ваш Telegram токен: " token
+export TF_VAR_telegram_token=$token
+
 
 echo "Переменные окружения установлены."
--- yc_set_env.ps1
+++ yc_set_env.ps1
@@ -1,6 +1,10 @@
+# Получение переменных из Yandex Cloud CLI
 $env:TF_VAR_cloud_id=$(yc config get cloud-id)
 $env:TF_VAR_folder_id=$(yc config get folder-id)
 $env:TF_VAR_yc_token=$(yc iam create-token)
-$env:TF_VAR_telegram_token=$TELEGRAM_BOT_TOKEN
+
+$token = Read-Host -Prompt "Введите ваш Telegram токен"
+$env:TF_VAR_telegram_token = $token
+
 
 Write-Host "Переменные окружения установлены."
